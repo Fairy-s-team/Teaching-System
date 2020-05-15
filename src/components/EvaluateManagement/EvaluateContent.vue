ac2e16b4c2464203f920ec2115b4d51b6aeec554
qiqi <1102598092@qq.com>
2020-05-15 18:36:07 +0800

    评价已完成

--- src/components/EvaluateManagement/EvaluateContent.vue
+++ src/components/EvaluateManagement/EvaluateContent.vue
@@ -102,7 +102,7 @@
   };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add "scoped" attribute to limit CSS to this component only  -->
 <style scoped>
   @import "../../assets/css/Evaluate.css";
 </style>
